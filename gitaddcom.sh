87adf0c56ebaedc69c0a0a5f8f43e2af2cbacc62
bboytian <94tian@gmail.com>
2020-06-23 18:30:38 +0800

    setting commit message to be at the end

--- gitaddcom.sh
+++ gitaddcom.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # SYNOPSIS
-# gitaddcom [<commit message>] [-a <files to add to index>] [-p]
+# gitaddcom [-a <files to add to index> --] [-p] [<commit message>] 
 
 
 ADDALLBOO=1
 PUSHBOO=0
 COMMITMSGBOO=0
 
-if ! [[ "$1" =~ ^-.* ]] && [[ "$#" -gt 0 ]]; then
-    COMMITMSGBOO=1
-    COMMITMSG=$1
-    shift
-fi   
+# if ! [[ "$1" =~ ^-.* ]] && [[ "$#" -gt 0 ]]; then
+#     COMMITMSGBOO=1
+#     COMMITMSG=$1
+#     shift
+# fi   
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
@@ -22,6 +22,11 @@ while [[ "$#" -gt 0 ]]; do
 		echo -e "\t $2"
 		git add $2
 		shift
+
+		if [[ "$2" == -- ]]; then
+		    shift
+		    break
+		fi
 	    done
 	    ;;
 	-p|--push)
@@ -30,6 +35,11 @@ while [[ "$#" -gt 0 ]]; do
 	-*)
 	    echo "invalid flag: $1"
 	    exit 1
+	    ;;
+	*)
+	    echo "this worked"
+	    COMMITMSGBOO=1
+	    COMMITMSG=$1
 	    ;;
     esac
 shift
