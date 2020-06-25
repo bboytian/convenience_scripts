66e8185d92a51d00ae89d91d2a7d93fb29c63409
bboytian <94tian@gmail.com>
2020-06-25 16:44:14 +0800

    sedrec.sh/added notes

--- sedrec.sh
+++ sedrec.sh
@@ -3,6 +3,10 @@
 # recursive sed -i on specified directory
 # this command is POWERFUL be very careful when using
 #
+# For sed argument:
+# to delete line containing input '/.*input.*/d'
+# to replace input with output 's/input/output/g'
+#
 # Sypnosis
 # sedrec <working dir> <sed expression>
 
