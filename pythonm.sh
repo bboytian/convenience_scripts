d5a20cedbc720f34bb6fa3de4bbe812fa7acf7a4
bboytian <94tian@gmail.com>
2020-07-13 17:43:49 +0800

    pythonm added run interactively

--- pythonm.sh
+++ pythonm.sh
@@ -3,16 +3,46 @@
 # usage:
 # 0. bash_alises created for this script
 # 1. cd to parent directory
-# 2. pythonm /path/to/fileORmodule
+# 2. pythonm [-i] /path/to/fileORmodule
 
-script_str=$1
+INTERACTIVEBOO=0
 
-if [ "${script_str:$(expr ${#script_str} - 1):1}" = '/' ] # py module
-then
-   script_str="${script_str:0:$(expr ${#script_str} - 1)}"
-elif [ "${script_str:$(expr ${#script_str} - 3):3}" = '.py' ] #py script
-then
-    script_str="${script_str:0:$(expr ${#script_str} - 3)}"
+# parsing arguments
+if [[ "$#" -gt 2 ]]; then
+    echo 'too many arguments'
+    exit 1
 fi
 
-echo $script_str | sed -e 's/\//\./g' | xargs python -m
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+	-i|--interactive)
+	    INTERACTIVEBOO=1
+	    ;;
+	-*)
+	    echo "invalid flag: $1"
+	    ;;
+	*)
+	    SCRIPTSTR=$1
+	    ;;
+    esac
+    shift
+done
+
+# parsing the directory for mudular input
+if [ "${SCRIPTSTR:$(expr ${#SCRIPTSTR} - 1):1}" = '/' ] # py module
+then
+   SCRIPTSTR="${SCRIPTSTR:0:$(expr ${#SCRIPTSTR} - 1)}"
+elif [ "${SCRIPTSTR:$(expr ${#SCRIPTSTR} - 3):3}" = '.py' ] #py script
+then
+    SCRIPTSTR="${SCRIPTSTR:0:$(expr ${#SCRIPTSTR} - 3)}"
+fi
+
+SCRIPTSTR=$(echo $SCRIPTSTR | sed -e 's/\//\./g')
+
+# running
+if [ $INTERACTIVEBOO == 1 ]; then
+    python -i -m $SCRIPTSTR
+else
+    python -m $SCRIPTSTR
+fi
