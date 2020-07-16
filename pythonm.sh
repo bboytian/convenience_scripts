69b7a524a724f6b93452edf5798ca946ff44a425
bboytian <94tian@gmail.com>
2020-07-16 12:07:04 +0800

    pythonm.sh allowed all flags

--- pythonm.sh
+++ pythonm.sh
@@ -3,32 +3,15 @@
 # usage:
 # 0. bash_alises created for this script
 # 1. cd to parent directory
-# 2. pythonm [-i] /path/to/fileORmodule
+# 2. pythonm [options] /path/to/fileORmodule
 
-INTERACTIVEBOO=0
-
-# parsing arguments
-if [[ "$#" -gt 2 ]]; then
-    echo 'too many arguments'
-    exit 1
-fi
-
-
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-	-i|--interactive)
-	    INTERACTIVEBOO=1
-	    ;;
-	-*)
-	    echo "invalid flag: $1"
-	    ;;
-	*)
-	    SCRIPTSTR=$1
-	    ;;
-    esac
+while [[ "$#" -gt 1 ]]; do
+    flags="$flags $1"
     shift
 done
 
+
+SCRIPTSTR=$1
 # parsing the directory for mudular input
 if [ "${SCRIPTSTR:$(expr ${#SCRIPTSTR} - 1):1}" = '/' ] # py module
 then
@@ -37,12 +20,8 @@ elif [ "${SCRIPTSTR:$(expr ${#SCRIPTSTR} - 3):3}" = '.py' ] #py script
 then
     SCRIPTSTR="${SCRIPTSTR:0:$(expr ${#SCRIPTSTR} - 3)}"
 fi
-
 SCRIPTSTR=$(echo $SCRIPTSTR | sed -e 's/\//\./g')
 
 # running
-if [ $INTERACTIVEBOO == 1 ]; then
-    python -i -m $SCRIPTSTR
-else
-    python -m $SCRIPTSTR
-fi
+python $flags -m $SCRIPTSTR
+
