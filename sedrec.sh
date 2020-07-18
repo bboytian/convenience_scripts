c0c5f1d049a1800754ad523fcb9246a1312587d9
bboytian <94tian@gmail.com>
2020-07-18 13:45:03 +0800

    sedrec.sh seemingly working recursive sed with path rejection flag

--- sedrec.sh
+++ sedrec.sh
@@ -10,52 +10,57 @@
 # Sypnosis
 # sedrec <working dir> [-x <exclude phrase> ... -- ] <sed expression>
 
-# if [[ "$#" -lt 2 ]]; then
-#     echo "missing arguments"
-#     exit 1
-# fi
-
-# WD=$1
-# shift
-
-# while [[ "$#" -gt 0 ]]; do
-#     case $1 in
-#	-x|--exclude)
-#	    echo 'excluding patterns in path...'
-#	    while ! [[ "$2" =~ ^-.* ]] && [[ "$#" -gt 1 ]]; do
-#		echo -e "\t excluding $2"
-#		if [ -z $REVGREPSTR ]; then
-#		    REVGREPSTR=$2
-#		else
-#		    REVGREPSTR="$REVGREPSTR\|'$2'"
-#		fi
-#		shift
-#	    done
-#	    ;;
-#	--)
-#	    ;;
-#	-*)
-#	    echo "invalid flag: $1"
-#	    exit 1
-#	    ;;
-#	*)
-#	    SEDSTR=$1
-#	    ;;
-#     esac
-#     shift
-# done
-
-# echo "$REVGREPSTR"
-# echo "find $WD -type f | grep -v -G "$REVGREPSTR""
-# echo
-# find $WD -type f | grep -v -G "$REVGREPSTR"
-
-
-# old code
 if [[ "$#" -lt 2 ]]; then
     echo "missing arguments"
     exit 1
-else
-    find "$1" \( -type d -name .git -prune \) -o -type f -print0 | \
-	xargs -0 sed -i "$2"
 fi
+
+XBOO=0
+WD=$1
+shift
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+	-x|--exclude)
+	    echo 'excluding patterns in path...'
+	    XBOO=1
+	    while ! [[ "$2" =~ ^-.* ]] && [[ "$#" -gt 1 ]]; do
+		echo -e "\t excluding $2"
+		if [ -z $REVGREPSTR ]; then
+		    REVGREPSTR=$2
+		else
+		    REVGREPSTR="$REVGREPSTR\|$2"
+		fi
+		shift
+	    done
+	    ;;
+	--)
+	    ;;
+	-*)
+	    echo "invalid flag: $1"
+	    exit 1
+	    ;;
+	*)
+	    SEDSTR=$1
+	    ;;
+    esac
+    shift
+done
+
+echo $(find $WD -type f)
+if [[ $XBOO == 1 ]]; then
+    find $WD -type f | grep -v -G "$REVGREPSTR" | xargs sed -i "$SEDSTR"
+else
+    echo came here
+    find $WD -type f | xargs sed -i "$SEDSTR"
+fi
+
+
+# old code
+# if [[ "$#" -lt 2 ]]; then
+#     echo "missing arguments"
+#     exit 1
+# else
+#     find "$1" \( -type d -name .git -prune \) -o -type f -print0 | \
+#	xargs -0 sed -i "$2"
+# fi
